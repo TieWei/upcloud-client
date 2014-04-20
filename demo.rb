dfc793e8981fe5c81392edbc777eaa56bb735e71
Wei Tie <nuaafe@gmail.com>
2014-04-20 09:48:54 +0800

    finish demo codes

--- demo.rb
+++ demo.rb
@@ -9,5 +9,31 @@ opts = {
 }
 
 bucket = "bucket"
-
+local_file = "localfile"
+filename = "filename"
+dir1 = "dir1"
+dir2 = "dir1/dir2/dir3"
 client = Upyun::Client.new(opts)
+
+
+
+
+client.mkdir(bucket, dir1)
+client.mkdir_p(bucket, dir2)
+
+client.rm(dir2)
+client.ls dir1
+client.info dir1
+client.show dir1
+
+client.create(bucket, filename, "#{dir2}/#{local_file}", :mkdir=>true)
+client.create(bucket, filename, "#{dir1}/#{local_file}", :mkdir=>true)
+
+client.show "#{dir2}/#{local_file}"
+client.show "#{dir1}/#{local_file}"
+client.get_file "#{dir2}/#{local_file}"
+client.read "#{dir2}/#{local_file}" do |content|
+	#do something
+end
+client.rm "#{dir2}/#{local_file}"
+client.rm "#{dir1}/#{local_file}"
